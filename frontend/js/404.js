53a9c52b23b4c8df19e6acecfe6ed682f537f419
Amin Lahbib <amine.lhb00@gmail.com>
2025-12-07 16:09:33 +0100

    Remove old frontend and update configuration for new React app

--- frontend/js/404.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const goToHomeButton = document.querySelector('.btn-primary');
-    if (goToHomeButton) {
-        goToHomeButton.addEventListener('click', function() {
-            window.location.href = './index.html'; // Redirect to the home page
-        });
-    }
-});
